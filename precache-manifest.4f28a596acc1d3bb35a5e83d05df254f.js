a28d7feed3af55a6e90cf9125dfcb724e11f5c12
joelbarenco@gmail.com <ebrosetup@gmail.com>
2018-12-18 10:50:28 +0000

    build from react app

---
+++ precache-manifest.4f28a596acc1d3bb35a5e83d05df254f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "0a5c5297db5884e506c2",
+    "url": "/static/css/main.f26a552e.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "e2e46a1799c067baecb7",
+    "url": "/static/js/1.e2e46a17.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0a5c5297db5884e506c2",
+    "url": "/static/js/main.0a5c5297.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "e2e46a1799c067baecb7",
+    "url": "/static/css/1.d11d48fb.chunk.css"
+  },
+  {
+    "revision": "33816ff3d4ae7952d7607f04e4a979af",
+    "url": "/index.html"
+  }
+];
